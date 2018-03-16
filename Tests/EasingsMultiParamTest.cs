bae4bb8d95faead54dc6669e3a0d79dc41511ce9
Maurycy Zarzycki <maurycy@evidentlycube.com>
2018-03-16 23:31:24 +0100

    Code reformat

--- Tests/EasingsMultiParamTest.cs
+++ Tests/EasingsMultiParamTest.cs
@@ -3,55 +3,68 @@ using System.Reflection;
 using Retromono.Easings;
 using Xunit;
 
-namespace Tests {
-    public class EasingsMultiParamTest {
-        public static IEnumerable<MethodInfo> GetEasings() {
-            var methods = typeof(EasingsMultiParam).GetMethods();
+namespace Tests
+{
+	public class EasingsMultiParamTest
+	{
+		public static IEnumerable<MethodInfo> GetEasings()
+		{
+			var methods = typeof(EasingsMultiParam).GetMethods();
 
-            foreach (var methodInfo in methods) {
-                if (!methodInfo.IsStatic || !methodInfo.IsPublic) {
-                    continue;
-                }
+			foreach (var methodInfo in methods)
+			{
+				if (!methodInfo.IsStatic || !methodInfo.IsPublic)
+				{
+					continue;
+				}
 
-                yield return methodInfo;
-            }
-        }
+				yield return methodInfo;
+			}
+		}
 
-        public static IEnumerable<double[]> GetFromToValues() {
-            yield return new double[] {0, 1};
-            yield return new double[] {10, 100};
-            yield return new double[] {100, -100};
-        }
+		public static IEnumerable<double[]> GetFromToValues()
+		{
+			yield return new double[] {0, 1};
+			yield return new double[] {10, 100};
+			yield return new double[] {100, -100};
+		}
 
-        public static IEnumerable<double> GetDurations() {
-            yield return 1;
-            yield return 100;
-        }
+		public static IEnumerable<double> GetDurations()
+		{
+			yield return 1;
+			yield return 100;
+		}
 
-        public static IEnumerable<object[]> GetTestParams() {
-            foreach (var methodInfo in GetEasings()) {
-                foreach (var fromToValue in GetFromToValues()) {
-                    foreach (var duration in GetDurations()) {
-                        yield return new object[] {methodInfo, fromToValue[0], fromToValue[1], duration};
-                    }
-                }
-            }
-        }
+		public static IEnumerable<object[]> GetTestParams()
+		{
+			foreach (var methodInfo in GetEasings())
+			{
+				foreach (var fromToValue in GetFromToValues())
+				{
+					foreach (var duration in GetDurations())
+					{
+						yield return new object[] {methodInfo, fromToValue[0], fromToValue[1], duration};
+					}
+				}
+			}
+		}
 
-        [Theory]
-        [MemberData(nameof(GetTestParams))]
-        public void ShouldReturnZeroAtZeroTime(MethodInfo methodInfo, double from, double to, double duration) {
-            var result = (double) methodInfo.Invoke(null, new object[] {0, from, to, duration});
+		[Theory]
+		[MemberData(nameof(GetTestParams))]
+		public void ShouldReturnZeroAtZeroTime(MethodInfo methodInfo, double from, double to, double duration)
+		{
+			var result = (double) methodInfo.Invoke(null, new object[] {0, from, to, duration});
 
-            Assert.Equal(from, result, 10);
-        }
+			Assert.Equal(from, result, 10);
+		}
 
-        [Theory]
-        [MemberData(nameof(GetTestParams))]
-        public void ShouldReturnOneAtEnd(MethodInfo methodInfo, double from, double to, double duration) {
-            var result = (double) methodInfo.Invoke(null, new object[] {duration, from, to, duration});
+		[Theory]
+		[MemberData(nameof(GetTestParams))]
+		public void ShouldReturnOneAtEnd(MethodInfo methodInfo, double from, double to, double duration)
+		{
+			var result = (double) methodInfo.Invoke(null, new object[] {duration, from, to, duration});
 
-            Assert.Equal(to, result, 10);
-        }
-    }
+			Assert.Equal(to, result, 10);
+		}
+	}
 }
--- Tests/EasingsTest.cs
+++ Tests/EasingsTest.cs
@@ -4,60 +4,66 @@ using System.Reflection;
 using Retromono.Easings;
 using Xunit;
 
-namespace Tests {
-    public class EasingsTest {
-        public static IEnumerable<object[]> GetEasings() {
-            var methods = typeof(Easings).GetMethods();
+namespace Tests
+{
+	public class EasingsTest
+	{
+		public static IEnumerable<object[]> GetEasings()
+		{
+			var methods = typeof(Easings).GetMethods();
 
-            foreach (var methodInfo in methods) {
-                if (!methodInfo.IsStatic || !methodInfo.IsPublic) {
-                    continue;
-                }
+			foreach (var methodInfo in methods)
+			{
+				if (!methodInfo.IsStatic || !methodInfo.IsPublic)
+				{
+					continue;
+				}
 
-                yield return new object[]{methodInfo};
-            }
-        }
+				yield return new object[] {methodInfo};
+			}
+		}
 
-        [Theory]
-        [MemberData(nameof(GetEasings))]
-        public void ShouldReturnZeroAtZeroTime(MethodInfo methodInfo) {
-            var result = (double)methodInfo.Invoke(null, new object[]{0});
-            
-            Assert.Equal(0, result, 10);
-        }
+		[Theory]
+		[MemberData(nameof(GetEasings))]
+		public void ShouldReturnZeroAtZeroTime(MethodInfo methodInfo)
+		{
+			var result = (double) methodInfo.Invoke(null, new object[] {0});
 
-        [Theory]
-        [MemberData(nameof(GetEasings))]
-        public void ShouldReturnOneAtEnd(MethodInfo methodInfo)
-        {
-            var result = (double)methodInfo.Invoke(null, new object[] { 1 });
+			Assert.Equal(0, result, 10);
+		}
 
-            Assert.Equal(1, result, 10);
-        }
+		[Theory]
+		[MemberData(nameof(GetEasings))]
+		public void ShouldReturnOneAtEnd(MethodInfo methodInfo)
+		{
+			var result = (double) methodInfo.Invoke(null, new object[] {1});
 
-        [Theory]
-        [MemberData(nameof(GetEasings))]
-        public void ShouldNeverReturnNanOrInfinityForValidValues(MethodInfo methodInfo)
-        {
-            for (var i = 1; i < 100; i++)
-            {
-                var result = (double)methodInfo.Invoke(null, new object[] { i / 100 });
+			Assert.Equal(1, result, 10);
+		}
 
-                Assert.True(!double.IsNaN(result));
-                Assert.True(!double.IsInfinity(result));
-            }
-        }
+		[Theory]
+		[MemberData(nameof(GetEasings))]
+		public void ShouldNeverReturnNanOrInfinityForValidValues(MethodInfo methodInfo)
+		{
+			for (var i = 1; i < 100; i++)
+			{
+				var result = (double) methodInfo.Invoke(null, new object[] {i / 100});
 
-        [Theory]
-        [MemberData(nameof(GetEasings))]
-        public void ShouldNeverReturnAnInsaneValue(MethodInfo methodInfo)
-        {
-            for (var i = 1; i < 100; i++)
-            {
-                var result = (double)methodInfo.Invoke(null, new object[] { i / 100.0 });
+				Assert.True(!double.IsNaN(result));
+				Assert.True(!double.IsInfinity(result));
+			}
+		}
 
-                Assert.InRange(result, -1, 2);
-            }
-        }
-    }
+		[Theory]
+		[MemberData(nameof(GetEasings))]
+		public void ShouldNeverReturnAnInsaneValue(MethodInfo methodInfo)
+		{
+			for (var i = 1; i < 100; i++)
+			{
+				var result = (double) methodInfo.Invoke(null, new object[] {i / 100.0});
+
+				Assert.InRange(result, -1, 2);
+			}
+		}
+	}
 }
